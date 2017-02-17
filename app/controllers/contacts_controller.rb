53be3db60d8d8d52927c86e3ecac52682df8bcb6
rymcmahon <toedragdangle9@gmail.com>
2017-02-17 11:30:01 -0600

    added pages controller and created contacts form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,20 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to '/pages/contact'
+    else
+      flash[:alert] = "Someting went wrong"
+      redirect_to '/pages/contact'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :email, :message)
+  end
 end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def care
+  end
+  def therapy
+  end
+  def contact
+  end
+end
