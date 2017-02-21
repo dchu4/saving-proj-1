53a4331b536cf6b2db9bb39f460109c76c1306a2
Daniel Chu <danchu123@gmail.com>
2017-02-21 15:57:04 -0600

    set up sendgrid for careers application

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,6 +1,8 @@
 include CareersHelper
 
 class CareersController < ApplicationController
+  # invisible_captcha only: [:create], honeypot: :subtitle
+
   def index
   end
 
@@ -13,6 +15,7 @@ class CareersController < ApplicationController
     @career = Career.new(career_params)
 
     if @career.save
+      ParamountMailer.job_application_email(@career).deliver_now
       redirect_to '/careers/thank_you'
     else
       render :new
