bb5cdce9773a4a7298defc998c911a0b0e81308f
Daniel Chu <danchu123@gmail.com>
2017-03-02 12:47:23 -0600

    Installed devise, Styled devise views to fit site

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'figaro'
 gem 'carrierwave'
 gem 'fog-aws'
 gem 'gon', '~> 6.1'
+gem 'devise', '~> 4.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
