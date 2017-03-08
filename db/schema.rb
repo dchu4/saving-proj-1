111cc887bb809a71c2fea8fc3a3a8bde8ef9cefa
Amy Chan <chamy09@gmail.com>
2017-03-08 13:43:18 -0600

    added authentication for edit buttons and sign in/out buttons

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20170308193413) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.boolean  "is_admin"
     t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
