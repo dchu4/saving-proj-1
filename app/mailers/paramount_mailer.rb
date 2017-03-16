929b3911cc4cee7d5891acbf69827bcafc1308f9
Daniel Chu <danchu123@gmail.com>
2017-03-16 12:06:20 -0500

    mailer for staging

--- app/mailers/paramount_mailer.rb
+++ app/mailers/paramount_mailer.rb
@@ -3,20 +3,20 @@ require 'open-uri'
 class ParamountMailer < ApplicationMailer
   def tour_email(tour)
     @tour = tour
-    mail(:from => @tour.email, :to => "danchu123@gmail.com", :bcc => @tour.email,
+    mail(:from => @tour.email, :to => "ryan@thedifferenceengine.io", :bcc => @tour.email,
     :subject => 'Tour Sign-up' )
   end
 
   def job_application_email(career)
     @career = career
     attachments[@career.resume.file.filename] = open(@career.resume.url).read
-    mail( :from => @career.email, :to => 'danchu123@gmail.com',
+    mail( :from => @career.email, :to => 'ryan@thedifferenceengine.io',
     :subject => "#{@career.job_role} Job Application")
   end
 
   def contact_email(contact)
     @contact = contact
-    mail(:from => @contact.email, :to => "danchu123@gmail.com",
+    mail(:from => @contact.email, :to => "ryan@thedifferenceengine.io",
     :subject => 'Paramount Contact' )
   end
 end
